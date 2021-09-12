d3c99e9ca3d395b1476ec4beadcf97cc7fcfe0cb
rifqiiardhian <rifqiardhian@gmail.com>
2021-09-12 20:03:21 +0700

    update remove GA tracker from tools, missing comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{--@if(env('APP_ENV')==='production')--}}
         {{--<!-- Global site tag (gtag.js) - Google Analytics -->--}}
-            {{-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156389913-2"></script>--}}
+            {{--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156389913-2"></script>--}}
             {{--<script>--}}
                 {{--window.dataLayer = window.dataLayer || [];--}}
                 {{--function gtag(){dataLayer.push(arguments);}--}}
