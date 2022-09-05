ded06bb69a086b7ca2e2ca6e3cc538ca1e3b0fb0
adioihsan <adioihsan@gmail.com>
2022-09-05 21:04:00 +0700

    add alert limit component

---
+++ resources/views/components/alert_limit.blade.php
@@ -0,0 +1,6 @@
+<div class="alert alert-limit d-flex justify-content-between" role="alert">
+    <div class=" d-flex align-items-center">
+        <i class="icon pr-2 bx bxs-error-circle bx-sm"></i> @lang('alert.alert-limit')
+    </div>
+    <a href="https://cmlabs.co/en-id/login" class="font-weight-bold">Login</a>
+</div>
