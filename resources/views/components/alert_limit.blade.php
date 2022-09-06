7571100409f4ad4b82a5eea164c7beb0cdc9f5e5
adioihsan <adioihsan@gmail.com>
2022-09-06 14:45:08 +0700

    fix alert_limit responsiveness

--- resources/views/components/alert_limit.blade.php
+++ resources/views/components/alert_limit.blade.php
@@ -1,6 +1,6 @@
-<div class="alert alert-limit d-flex justify-content-between" role="alert">
-    <div class=" d-flex align-items-center">
+<div class="alert alert-limit d-flex justify-content-between align-items-center" role="alert">
+    <div class=" d-flex align-items-center mr-2">
         <i class="icon pr-2 bx bxs-error-circle bx-sm"></i> @lang('alert.alert-limit')
     </div>
-    <a href="https://cmlabs.co/en-id/login" class="font-weight-bold">Login</a>
+    <a href="https://cmlabs.co/en-id/login">Login</a>
 </div>
