5678af01e346003bbc1e34a374128a4f45ec83c4
Rian Febriansyah <rianfebriansyah22@gmail.com>
2023-07-21 15:44:40 +0700

    adjust copy en and id

---
+++ resources/lang/en/plagiarism.php
@@ -0,0 +1,78 @@
+<?php
+return [
+    "meta-title" => "",
+    "meta-desc" => "",
+
+    "account_usage" => "ACCOUNT USAGE",
+    "team_usage" => "Team Usage",
+    "account_usage_2" => "Account Usage",
+    "cummulative_usage" => "CUMMULATIVE USAGE",
+
+    "your_request" => "YOUR REQUEST",
+    "request" => "REQUEST",
+    "words_checked" => "WORDS CHECKED",
+    "cost" => "COST",
+    "users" => "USERS",
+    "team_request" => "TEAM REQUEST",
+    "total_words" => "TOTAL WORDS",
+    "words" => "WORDS",
+    "times" => "times",
+
+    "title" => "PLAGIARISM CHECKER",
+    "desc" => "Plagiarism detection solution powered by CopyScape",
+
+    "results" => "RESULTS",
+
+    "characters" => "Characters",
+    "words" => "Words",
+    "sentences" => "Sentences",
+    "paragraph" => "Paragraph",
+    "reading_time" => "Reading Time",
+    "estcost" => "EST.COST",
+    "cancel" => "Cancel",
+    "run" => "Run",
+    "run_copyscape" => "Run CopyScape",
+    "cost_desc" => "$0.03 first 200 words, $0.01 next 100 words (about $0.05 for 400-449 words)",
+
+    "plc_text" => "Type / paste any text here..",
+    "plc_link" => "Type / paste url here (eg. https://cmlabs.co/)",
+    "empty" => "Enter the text you want to check and run to see the result here...",
+    "empty_history" => "This is your first impressions, no history yet!",
+
+    "url_valid" => "URL is Valid",
+
+    "plagiat" => "Menjiplak",
+    "unique" => "Unique",
+    "overview" => "Overview",
+    "view_full" => "Overview",
+
+    "history" => "History",
+    "my_acc" => "My Account",
+    "all_acc" => "All Account",
+    "download" => "Download (*.csv) File",
+
+    "whatsnew" => "What's New",
+    "other_tools" => "Other SEO Tools",
+    "last_update" => "Last update",
+    "update_desc" => "Now users can use the Word Counter tool with the autosave feature. With these updates and additional features, you don't have to worry if your writing gets lost when a technical problem occurs. Your writing would be saved safely on our system. It's time to write securely for better writing results.",
+    "notif_center" => "Notification center",
+
+    "cmlabs_alliance" => "cmlabs Aliance",
+    "cmlabs_alliance-desc" => "These strategic alliances allow us to offer our clients a wider range of SEO innovative solutions and exceptional service around the world. If you've ever planned of conquering the more market, you've come to the right place! <a href='https://cmlabs.co/en-id/seo-guidelines' class='text-underline'>Let's turn those plan into reality, shall we?</a>",
+    "writing_starter" => "Writing Starter Guide",
+    "writing_starter_desc" => "The Search Engine Optimization (SEO) Starter Guide provides best practices to make it easier for search engines to crawl, index, and understand your content.",
+
+    "story_lab-desc" => '<span class="green-story-labs">Gain more traffic</span> for your business',
+
+    "seo_services" => "SEO Services",
+    "media_buying" => "Digital Media Buying",
+    "content_writing" => "SEO Content Writing",
+    "political" => "SEO Political Campaign",
+    "backlink" => "Backlink Services",
+    "service_desc" => "Get a personalized SEO service and give your business a treat.",
+    "explore_now" => "Explore now",
+    "explore_today" => "Explore Today!",
+
+    "broaden" => "Broaden your SEO knowledge",
+    "launch" => "LAUNCH",
+];
--- resources/views/Tools/plagiarism-checker/components/stats.blade.php
+++ resources/views/Tools/plagiarism-checker/components/stats.blade.php
@@ -3,28 +3,28 @@
         <div class="col-12 col-lg-6 px-5 pt-4 pb-4 background-white">
             <div class="d-flex gap-4 text-dark-50">
                 <i class='bx bxs-user'></i>
-                <div class="b2-700 b2-m-700 ml-2">ACCOUNT USAGE</div>
+                <div class="b2-700 b2-m-700 ml-2">@lang('plagiarism.account_usage')</div>
             </div>
             <div class="row mt-2 text-dark-70">
                 <div class="col-4">
                     <p class="s-400 s-m-400 m-0">
-                        YOUR REQUEST
+                        @lang('plagiarism.your_request')
                     </p>
                     <p class="b1-700 b1-m-700 m-0">
-                        {{ $userSummaryLogs->user_requests }} times
+                        {{ $userSummaryLogs->user_requests }} @lang('plagiarism.times')
                     </p>
                 </div>
                 <div class="col-4">
                     <p class="s-400 s-m-400 m-0">
-                        WORDS CHECKED
+                        @lang('plagiarism.words_checked')
                     </p>
                     <p class="b1-700 b1-m-700 m-0">
-                        {{ $userSummaryLogs->total_words ?? '0' }} WORDS
+                        {{ $userSummaryLogs->total_words ?? '0' }} @lang('plagiarism.words')
                     </p>
                 </div>
                 <div class="col-4">
                     <p class="s-400 s-m-400 m-0">
-                        COST
+                        @lang('plagiarism.cost')
                     </p>
                     <p class="b1-700 b1-m-700 m-0">
                         ${{ $userSummaryLogs->total_cost ?? '0' }}
@@ -35,30 +35,30 @@
         <div class="col-12 col-lg-6 background-dark-70 text-white px-5 pt-4 pb-4">
             <div class="d-flex gap-4">
                 <i class='bx bxs-group text-white'></i>
-                <div class="b2-700 b2-m-700 ml-2">CUMMULATIVE USSAGE</div>
+                <div class="b2-700 b2-m-700 ml-2">@lang('plagiarism.cummulative_usage')</div>
             </div>
             <div class="mt-2 d-flex justify-content-between">
                 <div class="">
                     <p class="s-400 s-m-400 m-0">
-                        TEAM REQUEST </p>
-                    <p class="b1-700 b1-m-700 m-0"> {{ $cummulativeSummaryLogs->team_requests }} times
+                        @lang('plagiarism.team_request') </p>
+                    <p class="b1-700 b1-m-700 m-0"> {{ $cummulativeSummaryLogs->team_requests }} @lang('plagiarism.times')
                     </p>
                 </div>
                 <div class="">
                     <p class="s-400 s-m-400 m-0">
-                        USERS </p>
+                        @lang('plagiarism.users') </p>
                     <p class="b1-700 b1-m-700 m-0"> {{ $cummulativeSummaryLogs->total_users }}
                     </p>
                 </div>
                 <div class="col-3 flex-shrink-0">
                     <p class="s-400 s-m-400 m-0">
-                        TOTAL WORDS </p>
-                    <p class="b1-700 b1-m-700 m-0"> {{ $cummulativeSummaryLogs->total_words }} WORDS
+                        @lang('plagiarism.total_words') </p>
+                    <p class="b1-700 b1-m-700 m-0"> {{ $cummulativeSummaryLogs->total_words }} @lang('plagiarism.words')
                     </p>
                 </div>
                 <div class="">
                     <p class="s-400 s-m-400 m-0">
-                        COST </p>
+                        @lang('plagiarism.cost') </p>
                     <p class="b1-700 b1-m-700 m-0"> ${{ $cummulativeSummaryLogs->total_cost }}
                     </p>
                 </div>
