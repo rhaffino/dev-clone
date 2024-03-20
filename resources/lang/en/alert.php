9fc2cae34e42a122c67bc5e60788dca6e7f0cf03
triabagus <triatop9@gmail.com>
2024-03-20 15:19:36 +0700

    [Mobile Friendly Test] Adjustment: Notification maintenance & btn submit disable

--- resources/lang/en/alert.php
+++ resources/lang/en/alert.php
@@ -1,4 +1,5 @@
 <?php
 return [
     "alert-limit" => "You have reached the usage limit of this tool. To see the result and use this tool for further activities, please log in / register yourself.",
+    "alert-notif" => "We regret to inform you that the tool is currently unavailable due to system maintenance until further notice.",
 ];
