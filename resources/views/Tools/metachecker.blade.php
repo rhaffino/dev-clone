cf032255146335de5a7aba14396e1d6ef9770080
Rian Febriansyah <rianfebriansyah22@gmail.com>
2024-05-31 14:18:31 +0700

    update js version

--- resources/views/Tools/metachecker.blade.php
+++ resources/views/Tools/metachecker.blade.php
@@ -446,7 +446,7 @@
     const META_CHECKER_URL = "{{route('api.analyze-meta')}}"
 </script>
 <script src="{{asset('js/logic/predifine-localstorage.js')}}"></script>
-<script src="{{asset('js/logic/metachecker.js')}}?0989"></script>
+<script src="{{asset('js/logic/metachecker.js')}}?09"></script>
 <script type="text/javascript">
     $('#toggle_button_writer').click();
 </script>
